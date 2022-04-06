3f145ce0020947de54ea915d4a1fbf4e60441e8e
Shivam Patel <shivampatel@156.236.21.198.tigernet.wifi.dyn.clemson.edu>
2022-04-06 15:47:11 -0400

    implement add item functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
+
+  const [item, setItem] = useState("")
+  const [itemArr, addItem] = useState(["An Item"]);
+
+  function handleItem(event){
+    const value = event.target.value;
+    setItem(value);
+  }
+
+  function handleClick(event) {
+    addItem((prevVal) => {
+      return [...prevVal, item];
+    });
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={handleItem} type="text" placeholder="Enter an item" value={item}/>
+        <button onClick={handleClick}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {itemArr.map(item => <li>{item}</li>)}
         </ul>
       </div>
     </div>

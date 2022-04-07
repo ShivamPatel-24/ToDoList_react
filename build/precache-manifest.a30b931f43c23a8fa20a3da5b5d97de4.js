107ccead0a13d9dc43a62543a416f6c691919881
Shivam Patel <shivampatel@Shivams-Air.attlocal.net>
2022-04-06 20:22:37 -0400

    build app

---
+++ build/precache-manifest.a30b931f43c23a8fa20a3da5b5d97de4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d87280d8ed0618b7732af3986d2c25fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9302e28affcbd310209f",
+    "url": "/static/js/2.17e1ab02.chunk.js"
+  },
+  {
+    "revision": "32270e76b50f0e0fad04",
+    "url": "/static/js/main.6c92fd6a.chunk.js"
+  },
+  {
+    "revision": "164da21e09e43c8bb0c6",
+    "url": "/static/js/runtime-main.aa0ae0cd.js"
+  }
+]);
